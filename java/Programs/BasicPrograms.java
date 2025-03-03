77c9f18068a66c7bbbd283231543d456b824fc91
lakshman kandula <lakshmanswamikandula7702@gmail.com>
2025-03-03 14:33:19 +0530

    updating notes

---
+++ java/Programs/BasicPrograms.java
@@ -0,0 +1,11 @@
+public class BasicPrograms {
+    public static void main(String args[]) {
+        int a = -2;
+        int b = 5;
+        System.out.println(a & b);
+
+        // System.out.println(b);
+
+    }
+
+}
